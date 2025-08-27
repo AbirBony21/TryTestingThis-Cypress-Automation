a50bf83a6192f82842a348cb69201b76a7e15345
AbirBony21 <abirbony@gmail.com>
2025-08-27 22:39:11 +0600

    allure-reporter-integrated

--- cypress.config.js
+++ cypress.config.js
@@ -1,5 +1,6 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require("cypress")
 const {downloadFile} = require('cypress-downloadfile/lib/addPlugin')
+const allureWriter = require('@shelex/cypress-allure-plugin/writer')
 
 module.exports = defineConfig({
   e2e: {
@@ -8,6 +9,16 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
       on('task', {downloadFile})
+      allureWriter(on, config)
+      return config
     },
+    env: {
+      // turn Allure on by default (you can also pass --env allure=true via CLI)
+      allure: true,
+      // optional tweaks:
+      // allureResultsPath: 'allure-results',
+      // allureReuseAfterSpec: true, // if other plugins use after:spec listener
+      // allureAddVideoOnPass: true  // attach videos for passed tests too
+    }
   },
 });
